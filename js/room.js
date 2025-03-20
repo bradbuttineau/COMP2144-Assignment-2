e624aa63f1438a105ca668c01fab6fff4993833d
Brad Buttineau <113149072+bradbuttineau@users.noreply.github.com>
2025-03-20 14:10:35 -0400

    added light and shadows

--- js/room.js
+++ js/room.js
@@ -2,6 +2,7 @@
 
 const createScene = async function () {
     const scene = new BABYLON.Scene(engine);
+
     /*CAMERA*/
     const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.Vector3(0, 0, 0));
     camera.attachControl(canvas, true);
@@ -10,7 +11,12 @@ const createScene = async function () {
     /*LIGHT*/
     const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
-
+    //make a shadow with direction of light
+    const lightShadow = new BABYLON.DirectionalLight("directional", new BABYLON.Vector3(0, -1, 1), scene);
+    lightShadow.position = new BABYLON.Vector3(0, 50, -100);
+    const shadowGenerator = new BABYLON.shadowGenerator(1024, lightShadow);
+    //edge shadow
+    shadowGenerator.usePoissonSampling = true;
 
     /*MESHES*/
 
@@ -20,12 +26,12 @@ const createScene = async function () {
 
     /*SKY*/
     //add skybox
-    const skybox = BABYLON.MeshBuilder.CreateBox("skyBox", { size: 150}, scene);
+    const skybox = BABYLON.MeshBuilder.CreateBox("skyBox", { size: 150 }, scene);
     //apply images
     const skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
     skyboxMaterial.backFaceCulling = false;
     skyboxMaterial.disableLighting = true;
-   
+
 
     skybox.infiniteDistance = true;
     skyboxMaterial.disableLighting = true;
