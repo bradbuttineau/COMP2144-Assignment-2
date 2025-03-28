1a2dc885d2e0b9154e71bc24ec872941fdc036b8
Brad Buttineau <113149072+bradbuttineau@users.noreply.github.com>
2025-03-28 16:01:16 -0400

    added itemCard Babylon javascript to project

--- js/room.js
+++ js/room.js
@@ -1,44 +1,97 @@
 
-
-const createScene = async function () {
-    const scene = new BABYLON.Scene(engine);
-
-    /*CAMERA*/
-    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.Vector3(0, 0, 0));
-    camera.attachControl(canvas, true);
+import * as GUI from '@babylonjs/gui'
 
 
-    /* SKY*/
+
+    /*Card 1*/
+    const createItemCard1= () => {
+     const card1= BABYLON.MeshBuilder.CreateBox("detailed-card", {height:3.2, width: 2, depth: 0.2});
+     card1.position= new BABYLON.Vector3(0,2,4);
+    /*plane*/
+     const plane = BABYLON.MeshBuilder.CreatePlane("plane", {height: 3.2, width: 2});
+     plane.position.z = -0.11;
+     plane.position.y = 0.1;
+     plane.parent= card1;
+     const advancedTexture = BABYLON.GUI.AdvanceDynamicTexture.createForMesh(plane);
+    /*panel*/
+     const panel= new BABYLON.GUI.StackPanel();
+     panel.verticalAlignment=0;
+     advancedTexture.addControl(panel);
+
+     const image = new BABYLON.GUI.Image("image","textures\WHMIS.jpg");
+     image.height = "600px";
+     image.paddingTop= 40;
+     image.paddingLeft= 40;
+     image.paddingRight= 40;
+     panel.addControl(image);
+
+     const title= new BABYLON.GIU.TextBlock("title");
+     title.text="WHIMS Tutorial";
+     title.color="black";
+     title.fontsize= 48;
+     title.height= "110px"
+     text.textHorizontalAlignment= 0;
+     text.textVerticalAlignment= 0;
+     title. paddingTop= 40;
+     title.paddingLeft=40;
+     title.paddingRight=40;
+     panel.addControl(title);
+
+     const description = new BABYLON.GIU.TextBlock("title");
+     description.fontFamily="Tahoma, sans-serif";
+     description.text="L bbb";
+     description.textWrapping= true;
+     description.color="black";
+     description.fontsize= 24;
+     description.height="660px";
+     description.textHorizontalAlignment=0;
+     description.textVerticalAlignment=0;
+     description.paddingTop=20;
+     description.paddingLeft=40;
+     description.paddingRight=40;
+     panel.addControl(description);
+
+    const button1= BABYLON.GUI.Button.CreateSimpleButton("button1", "Toggle favorite");
+    button1.width=1;
+    button1.height="100px";
+    button1.color="black";
+    button1.backgroundColor="C62a88";
+    button1.fontsize=50;
+    button1.paddingBottom=20;
+    button1.paddingLeft=40;
+    button1.paddingRight=40;
+    button1.onPointerUpObservable.add(function (){
+        console.log("button1 clicked");
+    });
+    button1.verticalAlignment=1;
+    advancedTexture.addControl(button1);
+    };
     
-    const skybox = BABYLON.MeshBuilder.CreateBox("skybox", { size: 150 }, scene);
-   
-    const skyboxMaterial = new BABYLON.StandardMaterial("skybox", scene);
-    skyboxMaterial.backFaceCulling = false;
-    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("./textures/skybox", scene);
-    
-    
-    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
-    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
-    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-    // STEP 7d: Set the skybox material property
-    skybox.material = skyboxMaterial;
+    const createScene = function () {
+        const scene = new BABYLON.Scene(engine);
+        
+        
+    /*CAMERA*/ 
+    const camera= new BABYLON.ArchRotateCamera("camera", -Math.PI /2, Math.PI/ 2.5, 3, new BABYLON.Vector3(0,0,0) );
+
+   camera.upperBetaLimit= Math.PI/ 2.2;
+   camera.lowerRadiusLimit=2;
+   camera.upperRadiusLimit=50;
+   camera.setPosition(new BABYLON.Vector3(0, 1.5, 0));
+   camera.setTarget(new BABYLON.Vector3(0, 2, 4));
+   camera.attachControl(CanvasCaptureMediaStreamTrack, true);
+
     /*LIGHT*/
-    const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-    light.intensity = 0.7;
-    //make a shadow with direction of light
-    const lightShadow = new BABYLON.DirectionalLight("directional", new BABYLON.Vector3(0, -1, 1), scene);
-    lightShadow.position = new BABYLON.Vector3(0, 50, -100);
-    const shadowGenerator = new BABYLON.shadowGenerator(1024, lightShadow);
-    //edge shadow
-    shadowGenerator.usePoissonSampling = true;
+    const light=  new BABYLON.HemisphericLight("light");
+    light.intensity= 0.7;
 
-    /*MESHES*/
+    createItemCard1();
 
 
     /*IMMERSIVE AR */
 
     if (BABYLON.WebXRSessionManager.IsSessionSupportedAsync("immersive-vr")) {
-        const xr = await scene.createDefaultXRExperienceAsync({
+        const xr = scene.createDefaultXRExperienceAsync({
             floorMeshes: [largeGround],
             optionalFeatures: true
         });
