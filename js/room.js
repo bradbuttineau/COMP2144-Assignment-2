b198154ef52f53703b6419ab827101647ad68d66
Brad Buttineau <113149072+bradbuttineau@users.noreply.github.com>
2025-03-28 18:26:44 -0400

    change scene

--- js/room.js
+++ js/room.js
@@ -3,68 +3,104 @@
 
 
 /*Card 1*/
-const createItemCard1 = () => {
-    const card1 = BABYLON.MeshBuilder.CreateBox("detailed-card", { height: 3.2, width: 2, depth: 0.2 });
-    card1.position = new BABYLON.Vector3(0, 2, 4);
-    /*plane*/
-    const plane = BABYLON.MeshBuilder.CreatePlane("plane", { height: 3.2, width: 2 });
-    plane.position.z = -0.11;
-    plane.position.y = 0.1;
-    plane.parent = card1;
-    const advancedTexture = BABYLON.GUI.AdvanceDynamicTexture.createForMesh(plane);
-    /*panel*/
-    const panel = new BABYLON.GUI.StackPanel();
-    panel.verticalAlignment = 0;
-    advancedTexture.addControl(panel);
 
-    const image = new BABYLON.GUI.Image("image", "textures\WHMIS.jpg");
-    image.height = "600px";
-    image.paddingTop = 40;
-    image.paddingLeft = 40;
-    image.paddingRight = 40;
-    panel.addControl(image);
+function createCards(scene, items) {
+    // Create the 3D UI manager
+    var anchor = new BABYLON.TransformNode("");
+    var manager = new BABYLON.GUI.GUI3DManager(scene);
+  
+    var panel = new BABYLON.GUI.PlanePanel();
+    panel.margin = 0.2;
+  
+    manager.addControl(panel);
+    panel.linkToTransformNode(anchor);
+    panel.position.z = -1.5;
+  
+    // Let's add some buttons!
+    var addButton = function (item) {
+      var button = new BABYLON.GUI.HolographicButton("orientation");
+      panel.addControl(button);
+      button.text = item.title;
+    };
+  
+    panel.blockLayout = true;
+    for (item of items) {
+      console.log(item);
+  
+      addButton(item);
+    }
+    panel.blockLayout = false;
+  }
+  
 
-    const title = new BABYLON.GIU.TextBlock("title");
-    title.text = "WHIMS Tutorial";
-    title.color = "black";
-    title.fontsize = 48;
-    title.height = "110px"
-    text.textHorizontalAlignment = 0;
-    text.textVerticalAlignment = 0;
-    title.paddingTop = 40;
-    title.paddingLeft = 40;
-    title.paddingRight = 40;
-    panel.addControl(title);
 
-    const description = new BABYLON.GIU.TextBlock("title");
-    description.fontFamily = "Tahoma, sans-serif";
-    description.text = "L bbb";
-    description.textWrapping = true;
-    description.color = "black";
-    description.fontsize = 24;
-    description.height = "660px";
-    description.textHorizontalAlignment = 0;
-    description.textVerticalAlignment = 0;
-    description.paddingTop = 20;
-    description.paddingLeft = 40;
-    description.paddingRight = 40;
-    panel.addControl(description);
 
-    const button1 = BABYLON.GUI.Button.CreateSimpleButton("button1", "Toggle favorite");
-    button1.width = 1;
-    button1.height = "100px";
-    button1.color = "black";
-    button1.backgroundColor = "C62a88";
-    button1.fontsize = 50;
-    button1.paddingBottom = 20;
-    button1.paddingLeft = 40;
-    button1.paddingRight = 40;
-    button1.onPointerUpObservable.add(function () {
-        console.log("button1 clicked");
-    });
-    button1.verticalAlignment = 1;
-    advancedTexture.addControl(button1);
-};
+
+
+
+
+
+
+
+
+
+
+
+  import SceneWrapper from "@/scenes/SceneWrapper.js";
+  ...
+  async mounted() {
+      await SceneWrapper.createScene(document.getElementById("bjsCanvas"));
+      SceneWrapper.sendStartButton(this.populate);
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*Card 2*/
 const createItemCard2 = () => {
     const card1 = BABYLON.MeshBuilder.CreateBox("detailed-card", { height: 3.2, width: 2, depth: 0.2 });
@@ -74,7 +110,7 @@ const createItemCard2 = () => {
     plane.position.z = -0.11;
     plane.position.y = 0.1;
     plane.parent = card1;
-    const advancedTexture = BABYLON.GUI.AdvanceDynamicTexture.createForMesh(plane);
+    const advancedTexture = AdvancedDynamicTexture.CreateForMesh(plane, 2 * 512, 3.4 * 512);
     /*panel*/
     const panel = new BABYLON.GUI.StackPanel();
     panel.verticalAlignment = 0;
